e20f6494a2c29c10f76585242b32bd03074c253e
mpa19 <marcperezarnaiz@gmail.com>
2020-05-18 14:01:05 +0200

    All simple test mult

--- Test/SpreadSheetTest.java
+++ Test/SpreadSheetTest.java
@@ -11,13 +11,8 @@ class SpreadSheetTest {
 
     @BeforeEach
     public void setUpSheet() {
-        //spreadSheet.SpreadSheet.put("a3", 42);
         put("a1", 5);
         put("a2", 6);
-        put("a3", SpreadSheet.mult("a1", "a2"));
-        put("a4", SpreadSheet.mult(get("a1"), get("a2")));
-
-
     }
 
     @Test
@@ -26,14 +21,50 @@ class SpreadSheetTest {
     }
 
     @Test
-    public void cell_has_value() {
-
+    public void mult_ref_ref() {
+        put("a3", mult("a1", "a2"));
         assertEquals(new SomeValue(30).getValue(), ((SomeValue) get("a3")).getValue());
     }
 
     @Test
-    public void cell1_has_value() {
+    public void mult_exp_exp() {
+        put("a3", mult(get("a1"), get("a2")));
+        assertEquals(new SomeValue(30).getValue(), ((SomeValue) get("a3")).getValue());
+    }
 
-        assertEquals(new SomeValue(30).getValue(), ((SomeValue) get("a4")).getValue());
+    @Test
+    public void mult_exp_ref() {
+        put("a3", mult(get("a1"), "a2"));
+        assertEquals(new SomeValue(30).getValue(), ((SomeValue) get("a3")).getValue());
+    }
+
+    @Test
+    public void mult_val_exp() {
+        put("a3", mult(5, get("a2")));
+        assertEquals(new SomeValue(30).getValue(), ((SomeValue) get("a3")).getValue());
+    }
+
+    @Test
+    public void mult_val_val() {
+        put("a3", mult(5, 6));
+        assertEquals(new SomeValue(30).getValue(), ((SomeValue) get("a3")).getValue());
+    }
+
+    @Test
+    public void mult_val_ref() {
+        put("a3", mult(5, "a2"));
+        assertEquals(new SomeValue(30).getValue(), ((SomeValue) get("a3")).getValue());
+    }
+
+    @Test
+    public void mult_ref_exp() {
+        put("a3", mult("a1", get("a2")));
+        assertEquals(new SomeValue(30).getValue(), ((SomeValue) get("a3")).getValue());
+    }
+
+    @Test
+    public void mult_ref_val() {
+        put("a3", mult("a1", 6));
+        assertEquals(new SomeValue(30).getValue(), ((SomeValue) get("a3")).getValue());
     }
 }
